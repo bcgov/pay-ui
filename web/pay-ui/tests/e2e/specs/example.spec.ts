13ee2d55c70bdcdbbb8c0ad76875f3c229200901
Travis Semple <travis8814@gmail.com>
2025-12-02 18:35:27 -0800

    Remove E2E tests and fix unit test setup

--- web/pay-ui/tests/e2e/specs/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test.describe('Example', () => {
-  test('should show mock account name', async ({ page }) => {
-    await page.goto('./protected')
-    await expect(page.getByText('Hello Playwright')).toBeVisible()
-  })
-})
