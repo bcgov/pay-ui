8b7ea93a963ddb7ff5af779b22fb438e55799c93
Travis Semple <travis8814@gmail.com>
2025-12-22 10:25:27 -0800

    refactor routing slip store

--- web/pay-ui/app/stores/routing-slip-store.ts
+++ web/pay-ui/app/stores/routing-slip-store.ts
@@ -3,6 +3,9 @@ import type {
   RoutingSlipAddress, Payment, SearchRoutingSlipParams
 } from '~/interfaces/routing-slip'
 import { SearchRoutingSlipTableHeaders } from '@/utils/constants'
+import { PaymentTypes } from '@/enums/payment-types'
+import type { RoutingSlipSchema } from '~/types/create-routing-slip'
+import { createEmptyCRSState, createEmptyPaymentItem } from '~/utils/create-routing-slip'
 
 const defaultParams: SearchRoutingSlipParams = {
   page: 1,
@@ -10,7 +13,7 @@ const defaultParams: SearchRoutingSlipParams = {
   total: Infinity
 }
 
-// This is in a share because it's used across the app in various components.
+// This is in a store because it's used across the app in various components.
 export const useRoutingSlipStore = defineStore('routing-slip-store', () => {
   const store = reactive({
     routingSlip: {} as RoutingSlip,
@@ -30,5 +33,67 @@ export const useRoutingSlipStore = defineStore('routing-slip-store', () => {
 
   return {
     store
+  }
+})
+
+export const useCreateRoutingSlipStore = defineStore('create-routing-slip-store', () => {
+  const state = reactive<RoutingSlipSchema>(createEmptyCRSState())
+  const loading = ref<boolean>(false)
+  const reviewMode = ref<boolean>(false)
+
+  const isCheque = computed<boolean>(() => state.payment.paymentType === PaymentTypes.CHEQUE)
+
+  const totalCAD = computed<string>(() => Object.values(state.payment.paymentItems)
+    .reduce((total, item) => {
+      const amount = Number(item?.amountCAD) || 0
+      return total + amount
+    }, 0).toFixed(2)
+  )
+
+  function addCheque() {
+    const newItem = createEmptyPaymentItem()
+    state.payment.paymentItems[newItem.uuid] = newItem
+  }
+
+  function removeCheque(uuid: string) {
+    /* eslint-disable-next-line @typescript-eslint/no-dynamic-delete */
+    delete state.payment.paymentItems[uuid]
+  }
+
+  function resetPaymentState() {
+    const newItem = createEmptyPaymentItem()
+    state.payment.isUSD = false
+    state.payment.paymentItems = { [newItem.uuid]: newItem }
+  }
+
+  function resetUSDAmounts() {
+    for (const uuid in state.payment.paymentItems) {
+      const item = state.payment.paymentItems[uuid]
+      if (item) {
+        item.amountUSD = ''
+      }
+    }
+  }
+
+  function $reset() {
+    const newState = createEmptyCRSState()
+    state.details = newState.details
+    state.payment = newState.payment
+    state.address = newState.address
+    loading.value = false
+    reviewMode.value = false
+  }
+
+  return {
+    state,
+    isCheque,
+    totalCAD,
+    reviewMode,
+    loading,
+    addCheque,
+    removeCheque,
+    resetPaymentState,
+    resetUSDAmounts,
+    $reset
   }
 })
