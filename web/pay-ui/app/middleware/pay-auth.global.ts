209730fa2529133a68c3c660f407522111e92aa6
Travis Semple <travis8814@gmail.com>
2026-01-12 16:14:47 -0800

    everything is locked to staff functionality

--- web/pay-ui/app/middleware/pay-auth.global.ts
+++ web/pay-ui/app/middleware/pay-auth.global.ts
@@ -1,7 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { isAuthenticated, authUser } = useConnectAuth()
   const rtc = useRuntimeConfig().public
-
   if (!isAuthenticated.value) {
     return navigateTo(`/auth/login?return=${rtc.baseUrl}${to.fullPath.slice(1)}`)
   } else if (to.meta?.allowedRoles && !to.meta.allowedRoles.some(role => authUser.value.roles.includes(role))) {
