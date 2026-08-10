7d722ce9ce9a0ab540af44374f1ab08064574151
Your Name <your.email@example.com>
2026-08-10 11:48:52 -0400

    test automation code for BCA product validations

--- test-automation/pages/bca-validation-page.js
+++ test-automation/pages/bca-validation-page.js
@@ -29,6 +29,13 @@ export class BcaValidationPage {
     this.bcaPaymentMethods = this.bcaCard.locator('.product-payment-icons')
     this.productCheckbox = page.locator('[type="checkbox"]')
     this.cancelButton = page.getByRole('button', { name: 'Cancel' })
+    this.BCAssessmentAccess = page.getByText('This account has access to BC Assessment.')
+    this.removeProduct = page.getByText(' Remove Product ')
+    this.closeButton = page.getByText(' Close ')
+    this.dateSubmitted = page.locator('[placeholder="Date Submitted"]')
+    this.nameFilter = page.locator('[id="name"]')
+    this.pendingReviewTab = page.getByRole('tab', { name: ' Pending Review ' })
+    this.noActiveAccountsText = page.getByText('No Active Accounts') 
   }
 
   async getBcaPaymentMethods() {
@@ -60,5 +67,37 @@ export class BcaValidationPage {
     await this.productCheckbox.first().check({ timeout: 60000 })
     await this.waitForTimeout(3000)
     await this.cancelButton.click({ timeout: 60000 })
+  }
+
+   async addRemoveBCAProduct() {
+    await this.accountName.nth(1).click({ timeout: 60000 })
+    await this.editProfileLink.click({ timeout: 60000 })
+    await this.accountInfoText.click({ timeout: 60000 })
+    await this.productsAndPaymentLink.click({ timeout: 60000 })
+    if(this.BCAssessmentAccess.isVisible({ timeout: 60000 })) {
+      await this.BCAssessment.click({ timeout: 60000 })
+       await this.page.waitForTimeout(3000)
+      await this.removeProduct.click({ timeout: 60000 })
+      await this.BCAssessment.click({ timeout: 60000 })
+      await this.page.waitForTimeout(3000)
+      await this.closeButton.click({ timeout: 60000 })
+      await this.page.waitForTimeout(3000)
+      await expect(this.BCAssessmentAccess).toBeVisible({ timeout: 60000 })
+
+    } else
+    {
+      await this.BCAssessment.click({ timeout: 60000 })
+      await this.page.waitForTimeout(3000)
+      await this.closeButton.click({ timeout: 60000 })
+      await this.page.waitForTimeout(3000)
+      await expect(this.BCAssessmentAccess).toBeVisible({ timeout: 60000 })
+    }
+  }
+
+  async noReviewTaskForBCAProduct(accountName) {
+    await this.pendingReviewTab.click({timeout: 10000})
+    await this.dateSubmitted.fill(new Date().toISOString().split('T')[0], {timeout: 10000})
+    await this.nameFilter.fill(accountName, {timeout: 10000})
+    await expect(this.noActiveAccountsText).toBeVisible({timeout: 10000})
   }
 }
