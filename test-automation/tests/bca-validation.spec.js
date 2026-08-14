96d4fe6e90e9bb73421f0c9afbf5e7e2ec365bb4
Your Name <your.email@example.com>
2026-08-14 11:15:06 -0400

    added code for BCA reports

--- test-automation/tests/bca-validation.spec.js
+++ test-automation/tests/bca-validation.spec.js
@@ -35,3 +35,11 @@ test('validate products and services flow', async ({ page, bcaValidationPage , l
   await loginPage.loginWithBCSC(process.env.TEST_USERNAME_BCSC, process.env.TEST_PASSWORD_BCSC)
   await bcaValidationPage.validateProductsandServices()
 })
+
+test('validate bca reports', async ({ page, bcaValidationPage , loginPage }) => {
+  console.log('Test: Current URL before navigation:', page.url())
+  console.log('Test: Cookies loaded:', (await page.context().cookies()).length)
+  await page.goto(process.env.BASE_URL || 'undefined')
+  await loginPage.loginWithBCSC(process.env.TEST_USERNAME_BCSC, process.env.TEST_PASSWORD_BCSC)
+  await bcaValidationPage.validateBcaReports()
+})
